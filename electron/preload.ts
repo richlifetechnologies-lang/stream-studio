400c3965226c9c184a449d757e37c7b37abe1341
richlifetechnologies-lang <richlifetechnologies@gmail.com>
2026-06-25 11:40:01 -0600

    feat: expose update-available IPC and openExternal to renderer

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,3 +1,14 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("isElectron", true);
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  onUpdateAvailable: (
+    callback: (info: { version: string; downloadUrl: string; releaseUrl: string }) => void
+  ) => {
+    ipcRenderer.on("update-available", (_event, info) => callback(info));
+  },
+  openExternal: (url: string) => {
+    ipcRenderer.send("open-external", url);
+  },
+});
