ff67430e74888b4a5e83354feefe25a858a3bfae
richlifetechnologies-lang <richlifetechnologies@gmail.com>
2026-06-25 11:27:06 -0600

    Update src/App.tsx: remove login page, auto audio sync engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
-import { useEffect } from "react";
-import { Router, Route, Switch, useLocation } from "wouter";
-import { hasCredentials } from "./lib/credentials";
-import SetupPage from "./pages/setup";
+import { Router, Route, Switch } from "wouter";
 import StreamPage from "./pages/stream";
 import SettingsPage from "./pages/settings";
 import PopoutPage from "./pages/popout";
 import Toaster from "./components/toaster";
-
-function AuthGuard({ children }: { children: React.ReactNode }) {
-  const [location, setLocation] = useLocation();
-  useEffect(() => {
-    if (!hasCredentials() && location !== "/setup") {
-      setLocation("/setup");
-    }
-  }, [location, setLocation]);
-  return <>{children}</>;
-}
 
 export default function App() {
   return (
     <Router>
       <Toaster />
       <Switch>
-        <Route path="/setup" component={SetupPage} />
         <Route path="/popout">
           <PopoutPage />
         </Route>
         <Route path="/settings">
-          <AuthGuard>
-            <SettingsPage />
-          </AuthGuard>
-        </Route>
-        <Route path="/">
-          <AuthGuard>
-            <StreamPage />
-          </AuthGuard>
+          <SettingsPage />
         </Route>
         <Route>
-          <AuthGuard>
-            <StreamPage />
-          </AuthGuard>
+          <StreamPage />
         </Route>
       </Switch>
     </Router>
